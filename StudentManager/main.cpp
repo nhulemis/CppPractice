0bf7fc747d40267ec8ab96797e1f3cb542b2b891
Nhu Le <nhulemis@gmail.com>
2018-12-31 11:43:48 +0700

    + Add struct Student

---
+++ StudentManager/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#pragma region declare function and properties
+
+#pragma endregion
+
+#pragma region Struct
+struct Student
+{
+	int _id;
+	string _name;
+	float _score;
+};
+
+#pragma endregion
+
+
+int main() {
+
+}
