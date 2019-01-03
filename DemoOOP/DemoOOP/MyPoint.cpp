22a1e7ca1dd8ba42e7f8a899218adbe57eee2fac
Nhu Le <nhulemis@gmail.com>
2019-01-03 12:48:15 +0700

    add properties add constructor with parameter add function Display

--- DemoOOP/DemoOOP/MyPoint.cpp
+++ DemoOOP/DemoOOP/MyPoint.cpp
@@ -1,12 +1,32 @@
 #include "MyPoint.h"
+#include <iostream>
 
+using namespace std;
 
+int mPosX;
+int mPosY;
 
 MyPoint::MyPoint()
 {
 }
 
 
+MyPoint::MyPoint(int x, int y) {
+	mPosX = x;
+	mPosY = y;
+}
+
+void MyPoint::Display()
+{
+	cout << "X : " << mPosX << "- Y : " << mPosY << endl;
+}
+
+
+
+
+
 MyPoint::~MyPoint()
 {
 }
+
+
--- DemoOOP/DemoOOP/MyPoint.h
+++ DemoOOP/DemoOOP/MyPoint.h
@@ -3,6 +3,8 @@ class MyPoint
 {
 public:
 	MyPoint();
+	MyPoint(int x, int y);
+	void Display();
 	~MyPoint();
 };
 
